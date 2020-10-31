785f43e0444eff0c00ad5af19aacd54f12fe788f
Ivan <gibson.ivan@gmail.com>
2020-10-31 14:05:12 -0400

    small changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 birthdayGif = document.querySelector(".birthdayGif");
-button = document.querySelector('button');
+button = document.querySelector('.button');
 
 async function getGifs() {
     const responce = await fetch(
-      `http://api.giphy.com/v1/gifs/search?api_key=25J4LHZveJnRn2TxEnvpBc5BMLLNEqQ7&q=birthday`
+      `https://api.giphy.com/v1/gifs/search?api_key=25J4LHZveJnRn2TxEnvpBc5BMLLNEqQ7&q=birthday`
     );
     const data = await responce.json();
     const gifs = [...data.data];
